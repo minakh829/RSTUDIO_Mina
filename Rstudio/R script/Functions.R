56bfd3eee2d0dd52b87bf0b8b1e001e2967be0c3
minakh829 <57958659+minakh829@users.noreply.github.com>
2023-02-20 19:20:16 +0100

    functions & Main script
    creating the functions

--- Rstudio/R script/Functions.R
+++ Rstudio/R script/Functions.R
@@ -1,3 +1,57 @@
 # this is the list of functions 
 
-triple<-function(x){x*3}
+check_weight<-function(df){
+  
+  df[["WGTBL"]]= ifelse(df[["WGTBLU"]]=="lb",df[["WGTBL"]]*0.45,df[["WGTBL"]])
+  df[["WGTBLU"]]= ifelse(df[["WGTBLU"]]=="lb","kg","kg")
+  return(df)
+}
+
+check_height<-function(df){
+  
+  df[["HGTBL"]]= ifelse(df[["HGTBLU"]]=="m" & df[["HGTBL"]]<2.5 ,df[["HGTBL"]],df[["HGTBL"]]*0.01)
+return(df)
+ 
+}
+
+
+preprocessing_ex1<-function(df){
+  
+  df<- df%>% dplyr::filter(PARAMCD=="C64849B",VISITNUM==10)
+  return(df)
+}
+
+
+preprocessing_ex2<-function(df){
+  
+  df<- df%>% dplyr::filter(PARAMCD=="C64849B",ANL01FL=="Y")
+  return(df)
+}
+
+remove_duplicate<-function(df,fL=TRUE){
+  
+  df<-df[which(duplicated(df$SUBJID,fromLast = fL)==FALSE),]
+  return(df)
+}
+
+
+
+
+#Main functions 
+
+
+ADSL_processed<-check_weight(ADSL)
+ADSL_processed<-check_height(ADSL_processed)
+ADLB_ex1<-preprocessing_ex1(ADLB)
+ADLB_ex2<-preprocessing_ex2(ADLB)
+ADLB_processed<-remove_duplicate(ADLB_ex1)
+
+
+
+
+#df1<-df1 %>% mutate(ANL01FL=tolower(ANL01FL))
+
+
+
+
+
--- Rstudio/R script/Scrip1.R
+++ Rstudio/R script/Scrip1.R
@@ -5,7 +5,7 @@ library(readxl)
 
 
 
-
+source("C://Users//Mina/Documents/GitHub//RSTUDIO_Mina//Rstudio//R script/Functions.R")
 
 
 #Loading the data
@@ -13,3 +13,4 @@ ADSL<- read_excel("C://Users//Mina/Documents/GitHub//RSTUDIO_Mina//Rstudio//Data
 ADLB<- read_excel("C://Users//Mina/Documents/GitHub//RSTUDIO_Mina//Rstudio//Data//data.xlsx", sheet = "ADLB")
 
 
+triple(2)
